eaadac5b482ed3a89016816275fd95eb8c9ba120
zyd134 <2602828321@qq.com>
2020-09-18 18:56:14 +0800

    1

--- src/main/java/com/zhang/error/ErrorConfig.java
+++ src/main/java/com/zhang/error/ErrorConfig.java
@@ -11,6 +11,7 @@ public class ErrorConfig implements ErrorPageRegistrar {
 
     @Override
     public void registerErrorPages(ErrorPageRegistry registry) {
+
         ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
         registry.addErrorPages(error404Page);
     }
